0dd9aee183881cf8de213f65702cbb0370acfc8c
Nidhey Nitin Indurkar <nidhey60@gmail.com>
2022-08-12 16:33:13 +0000

    v2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const _PORT = process.env.PORT || 3000;
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     status: true,
-    message: "Nodejs Docker App v1",
+    message: "Nodejs Docker App v2",
   });s
 });
 
